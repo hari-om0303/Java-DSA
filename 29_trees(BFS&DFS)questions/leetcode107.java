482a6bbd2e0e699bc654c7c18ac6beae6f5a020e
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-01-07 18:42:59 +0530

    leetcode 107 level order bottom done

--- 29_trees(BFS&DFS)questions/leetcode107.java
+++ 29_trees(BFS&DFS)questions/leetcode107.java
@@ -1,9 +1,57 @@
+import java.util.*;
+
 // Java Program to Illustrate First Argument
 class leetcode107{
 
-    public static void main(String[] args) {
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+     TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+ }
 
-        // Printing the first argument
-        System.out.println(args[0]);
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null){
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i=0; i<size; i++){
+              TreeNode node = queue.poll();
+              level.add(node.val);
+
+              if (node.left != null) queue.add(node.left);
+              if (node.right != null) queue.add(node.right);
+            }
+          result.add(level);
+        }
+        Collections.reverse(result);
+    return result;
+    }
+
+    public static void main(String[] args) {
+        leetcode107 obj = new leetcode107();
+
+        TreeNode root =
+                new TreeNode(3,
+                        new TreeNode(9),
+                        new TreeNode(20,
+                                new TreeNode(15),
+                                new TreeNode(7)));
+
+        System.out.println(obj.levelOrderBottom(root));
     }
 }
