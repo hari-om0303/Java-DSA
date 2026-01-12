b0ef09bac73da7f213f1755f38abfd1cd7d7f229
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-01-12 17:11:54 +0530

    hashmap example

---
+++ 30_hashing_by_apna_college/src/hashmap/hashmap_example.java
@@ -0,0 +1,36 @@
+package hashmap;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class hashmap_example {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+//        insertinon
+        set.add(45);
+        set.add(50);
+        set.add(20);
+        set.add(45);
+        set.add(22);
+
+        System.out.println("size of hashset is :" + set.size());
+
+//        search
+        if (set.contains(50)){
+            System.out.println("🥰🥰🥰");
+        }
+
+//        delete
+        set.remove(45);
+        System.out.println(set);
+
+        Iterator it = set.iterator();
+        while (it.hasNext()){
+            System.out.print(it.next() + ",");
+        }
+
+        if (set.isEmpty()){
+            System.out.println("set is empty");
+        }
+    }
+}
